9f15fb20c49ccabe568b036442a4335cfece41f9
Sebastian Słoniewski <s.sloniewski99@gmail.com>
2024-11-13 00:53:22 +0100

    first tempaltes

--- mytodo/settings.py
+++ mytodo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2(d=560t@3mnv-rxdd-@fwc)h2g244_wmp4$xapt1yr&qpp$4q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'localhost', '127.0.0.1', '.mytodo.pl']
+ALLOWED_HOSTS = [ 'https://slon09.pythonanywhere.com', 'localhost', '127.0.0.1', '.mytodo.pl']
 
 
 # Application definition
